dd3b41493521851b2303b5aef7e829672c4e47c9
avinashbattula19-dotcom <avinashbattula19@gmail.com>
2026-07-30 20:25:06 +0530

    Time: 0 ms (100%), Space: 8.9 MB (94.35%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,14 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    
+    int i=0;
+        while(i<numsSize){
+            if(nums[i]>=target){
+                return i;
+            }
+            i++;
+        
+            
+        }
+    
+    return numsSize;
+}
